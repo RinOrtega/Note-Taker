e0ee8eafe39ad9cd511fa7b7cbd509ba3d39f6a5
ortegaerin@icloud.com <ortegaerin@icloud.com>
2024-04-21 23:47:08 -0500

    First commit for Heroku

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const express = require('express');
 const html_routes = require('./Routes/html-routes');
 const api_routes = require('./Routes/api-routes');
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(html_routes);
-app.use(api_routes);
+app.use(express.static("public"));
+app.use(html_routes)
+app.use(api_routes)
 
 app.listen(PORT, () => {
-    console.log(' On http://localhost:${PORT}');
+    console.log('Server running on http://localhost:${PORT}');
 });
